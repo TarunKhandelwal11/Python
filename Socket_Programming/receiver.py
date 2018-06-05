71cccf40bdb0c3ebac4b7a942f14b2b4e83f0e8b
root <root@localhost.localdomain>
2018-06-05 23:45:52 +0530

    For Command Output

---
+++ Socket_Programming/receiver.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python2
+
+#import modeule
+import socket
+import os
+import commands
+
+#for make connection between sender and receiver
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+#for binding the network 
+s.bind(("127.0.0.1",7777))
+
+for i in range(10000):
+	#to get the data from sender side
+	data = s.recvfrom(10000)
+	#to get the output of command
+	check=commands.getstatusoutput(data[0])
+	if check==0:
+		#to send the command output to sender
+		s.sendto(check[1],data[1])
+	else:
+		#to send the output if command not found 
+		s.sendto(check[1],data[1])
